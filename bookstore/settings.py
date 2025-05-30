5d40f7ac3b26329ca1ea33de534496b5b52c51ff
Fernando Bordin <fernandobordin07@hotmail.com>
2025-05-30 10:11:46 -0300

    Corrigindo autenticação da tela de Order, authenticação da tela exige senha do admin

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -145,6 +145,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
